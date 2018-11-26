161b7e590c2f691ca821263ff646cfc038188499
John (Kyungho) Min <37623568+john0227@users.noreply.github.com>
2018-11-26 14:06:45 +0800

    Update Projectile.py
    Made use of the Processing built-in function, dist()

--- Projectile.py
+++ Projectile.py
@@ -12,20 +12,18 @@ class Projectile:
     
     def move(self):
         # Normalize vector
-        d = sqrt(pow(self.starting[0] - self.ending[0], 2) + pow(self.starting[1] - self.ending[1], 2))
-        v = ((self.ending[0] - self.starting[0]) / d, (self.ending[1] - self.starting[1]) / d)
+        m = dist(self.starting[0], self.starting[1], self.ending[0], self.ending[1])
+        v = ((self.ending[0] - self.starting[0]) / m, (self.ending[1] - self.starting[1]) / m)
         # Set dx and dy
         dx = v[0] * self.spd
         dy = v[1] * self.spd
-        d = self.distance(self.starting[0], self.starting[1], self.x + dx, self.y + dy)
-        D = self.distance(self.starting[0], self.starting[1], self.ending[0], self.ending[1])
+        d = dist(self.starting[0], self.starting[1], self.x + dx, self.y + dy)
+        D = dist(self.starting[0], self.starting[1], self.ending[0], self.ending[1])
         if d > D:
             dx, dy = self.ending[0] - self.x, self.ending[1] - self.y
         # Move position
         self.x += dx
         self.y += dy
-    def distance(self, x1, y1, x2, y2):
-        return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2))
     def display(self):
         self.move()
         # Draw the projectile
